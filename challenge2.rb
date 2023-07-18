34e094eea28024ca4b0e67d9fe1ea35f8131325a
Miranda Delaney <delaney.miranda1@gmail.com>
2023-07-18 13:32:40 -0600

    Add challenge2.rb answer

--- challenge2.rb
+++ challenge2.rb
@@ -1,1 +1,11 @@
-1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+# 2. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+# Over-all goal: Write an array of strings (uppercase and lowercase). Write a method that applies lowercase to all strings and prints only in lowercase.
+# Psuedocode: Write an array of strings called names, apply an each method of lowercase to the names and a puts statement to print them in all lowercase.
+
+names = ["Chad", "Blaine", "Miranda", "Mark", "Dave"]
+
+names.each do |name|
+    puts name.downcase
+end
+
